65fe1a7c43519fc10f46930d109600c5551e2099
Gianluca Ghislotti <g.ghislotti@campus.unimib.it>
2024-04-29 22:27:38 +0200

    Project, contact and footer sections added

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 120);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navvlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navvlist.classList.toggle('activate');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navvlist.classList.remove('activate');
+};
